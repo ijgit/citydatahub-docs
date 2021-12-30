7648cd0f8a68f0cfeeb339b6b2701c835dd91e79
ijgit <isuu.jeong@gmail.com>
2021-12-30 12:05:15 +0900

    make new project

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'City Data-Hub',
-  tagline: '지속 가능한 도시성장과 삶의 질 향상을 위한 데이터 기반의 스마트 시티를 실현하는 개방형 데이터 허브 아키텍처 및 핵심 기술',
-  url: 'https://docs.citydatahub.kr',
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -18,15 +18,19 @@ const config = {
 
   presets: [
     [
-      '@docusaurus/preset-classic',
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          
           sidebarPath: require.resolve('./sidebars.js'),
-          routeBasePath: '/docs',
           // Please change this to your repo.
-          // editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -37,62 +41,77 @@ const config = {
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    
     ({
-      colorMode:{
-        defaultMode: 'light',
-        disableSwitch: true,
-      },
       navbar: {
-        // title: 'My Site',
-        hideOnScroll: true,
+        title: 'My Site',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.png',
+          src: 'img/logo.svg',
         },
         items: [
           {
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Document',
+            label: 'Tutorial',
           },
-          // {
-          //   type: 'search',
-          //   position: 'right',
-          // }
-          // {to: '/blog', label: 'Blog', position: 'left'},
-          // {
-          //   label: 'Docs',
-          //   position: 'left', // or 'right'
-          //   items: [
-          //     {
-          //       label: 'Category 1',
-          //       to: '/Document',
-          //     },
-          //     {
-          //       label: 'Category 2',
-          //       to: '...',
-          //     },
-          //   ],
-          // },
+          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'http://citydatahub.kr/',
-            label: 'City Data-hub',
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
             position: 'right',
           },
         ],
       },
       footer: {
-        style: 'light',
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['bash']
       },
-
-
     }),
 };
 
